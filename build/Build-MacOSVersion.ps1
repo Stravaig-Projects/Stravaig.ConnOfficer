79347d1996d22573d5d182ef6950315fd96079fe
Colin Mackay <github@colinmackay.scot>
2024-07-05 16:55:08 +0100

    (#2) Build Mac and Windows Applications

--- build/Build-MacOSVersion.ps1
+++ build/Build-MacOSVersion.ps1
@@ -1,3 +1,23 @@
 # TODO: Build the MacOS version
 
-Write-Host "This script builds the MacOS version of the applicatiln."
+Write-Host "This script builds the MacOS version of the application."
+
+$projectName = $ENV:STRAVAIG_PROJECT;
+$projectPath = "./src/$projectName/$projectName.csproj"
+
+$baseDir = "./out";
+$programDir = "$baseDir/Stravaig Conn Officer.app";
+$contentsDir = "$programDir/Contents";
+$codeSignatureDir = "$contentsDir/_CodeSignature";
+$codeResourcesDir = "$codeSignatureDir/CodeResources";
+
+New-Item -ItemType Directory -Force -Path $codeResourcesDir;
+
+$macOSDir = "$contentsDir/MacOS";
+New-Item -ItemType Directory -Force -Path $MacOSDir;
+
+$resourcesDir = "$contentsDir/Resources";
+New-Item -ItemType Directory -Force -Path $resourcesDir;
+
+dotnet publish $projectPath --runtime osx-x64 --configuration Release -p:UseAppHost=true --output $macOSDir --self-contained true -p:PublishSingleFile=true
+
