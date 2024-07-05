a61563d1c81ba00f397dc9365525018ab0102ea9
Colin Mackay <github@colinmackay.scot>
2024-07-06 00:11:48 +0100

    (#2) Build icons

--- build/Build-MacOSVersion.ps1
+++ build/Build-MacOSVersion.ps1
@@ -3,21 +3,30 @@
 Write-Host "This script builds the MacOS version of the application."
 
 $projectName = $ENV:STRAVAIG_PROJECT;
-$projectPath = "./src/$projectName/$projectName.csproj"
+$projectPath = "./src/$projectName/$projectName.csproj";
+$appIconSet = "./src/$projectName/MacOS/icon.iconset";
+$infoPListPath = "./src/$projectName/MacOS/Info.plist";
 
-$baseDir = "./out";
-$programDir = "$baseDir/Stravaig Conn Officer.app";
+$outputDir = "./out";
+$programDir = "$outputDir/Stravaig Conn Officer.app";
 $contentsDir = "$programDir/Contents";
 $codeSignatureDir = "$contentsDir/_CodeSignature";
 $codeResourcesDir = "$codeSignatureDir/CodeResources";
-
-New-Item -ItemType Directory -Force -Path $codeResourcesDir;
-
 $macOSDir = "$contentsDir/MacOS";
-New-Item -ItemType Directory -Force -Path $MacOSDir;
-
 $resourcesDir = "$contentsDir/Resources";
-New-Item -ItemType Directory -Force -Path $resourcesDir;
+$iconResourcePath = "$resourceDir/ConnOfficerAppIcon.icns";
 
+Write-Host ""
+Write-Host "Creating application directory structure";
+$null = New-Item -ItemType Directory -Force -Path $codeResourcesDir;
+$null = New-Item -ItemType Directory -Force -Path $MacOSDir;
+$null = New-Item -ItemType Directory -Force -Path $resourcesDir;
+
+Write-Host ""
+Write-Host "Converting $appIconSet to $iconResourcePath";
+iconutil -c icns --output "$iconResourcePath" "$appIconSet"
+
+
+Write-Host ""
+Write-Host "Publishing application";
 dotnet publish $projectPath --runtime osx-x64 --configuration Release -p:UseAppHost=true --output $macOSDir --self-contained true -p:PublishSingleFile=true
-
