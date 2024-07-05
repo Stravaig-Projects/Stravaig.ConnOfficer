ba9aa96efbc678e3870d5b593f522eb4aa067792
Colin Mackay <github@colinmackay.scot>
2024-07-05 16:55:08 +0100

    (#2) Build Mac and Windows Applications

--- build/Build-WindowsVersion.ps1
+++ build/Build-WindowsVersion.ps1
@@ -1,3 +1,10 @@
 # TODO: Build the windows version
 
 Write-Host "This script builds the windows version of Stravaig Conn Officer"
+
+$projectName = $ENV:STRAVAIG_PROJECT;
+$projectPath = "./src/$projectName/$projectName.csproj"
+
+$baseDir = "./out";
+
+dotnet publish $projectPath --runtime win-x64 --configuration Release --output $baseDir --self-contained true -p:PublishSingleFile=true
