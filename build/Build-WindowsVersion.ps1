f95467f77d97ef6bdec90440990afdea44db9eb3
Colin Mackay <github@colinmackay.scot>
2024-07-06 22:34:57 +0100

    Create Windows Files artefact before installer

--- build/Build-WindowsVersion.ps1
+++ build/Build-WindowsVersion.ps1
@@ -8,17 +8,24 @@ Log "This script builds the windows version of Stravaig Conn Officer"
 
 $repoRoot = [System.IO.Path]::GetFullPath("$PSScriptRoot/..");
 $projectName = "Stravaig.ConnOfficer"
-$projectPath = "$repoRoot/src/$projectName/$projectName.csproj"
+$projectFolder = "$repoRoot/src/$projectName";
+$projectPath = "$projectFolder/$projectName.csproj"
 
 $workingDir = Get-Location;
 $outputDir = "$workingDir/out";
 if (Test-Path $outputDir)
 {
-    Log("Cleaning up the output directory")
-    Remove-Item $outputDir -Recurse -Force
+    Log "Cleaning up the output directory"
+    $null = Remove-Item $outputDir -Recurse -Force
+}
+else
+{
+    $null = New-Item -ItemType Directory -Force -Path $outputDir;
 }
 
+Log "Publishing application"
 dotnet publish $projectPath --runtime win-x64 --configuration Release --output $outputDir --self-contained true -p:PublishSingleFile=true
 
+Log "Cleaning up"
 Remove-Item "$outputDir/*.pdb" -Force
 Remove-Item "$outputDir/*.xml" -Force
